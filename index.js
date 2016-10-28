1d3c622e4a3973fe49f0c35c653dd0365b973e85
SheaClose <SheaClose@gmail.com>
2016-10-28 17:53:59 -0500

    added User schema, Ctrls, routes, added mongoose

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const express = require("express")
       , session = require('express-session')
 			, mongoose = require("mongoose")
 			, mongoUri = serverConfig.mongoUri
+			, masterRoutes = require("./server/masterRoutes.js")
 
 app.use(session(serverConfig.session) );
 app.use("/", express.static(__dirname));
 app.use(json());
 app.use(cors());
 mongoose.connect(mongoUri);
-
+masterRoutes(app)
 
 app.listen(port, () => {console.log(`This is Dr. Crane... I'm listening. Port:${port}`)})
